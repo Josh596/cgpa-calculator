672dcc4819ee5db49250b8bd6babffbc509a2b42
Josh596 <64589157+Josh596@users.noreply.github.com>
2023-02-05 21:50:03 +0100

    Minor change

--- src/components/SemesterButton.js
+++ src/components/SemesterButton.js
@@ -3,10 +3,10 @@ import { BsFillTrashFill } from 'react-icons/bs'
 
 function SemesterButton({ onClick, active, id, handleDeleteSemester }) {
   return (
-    <div className={`rounded-full border ${active ? "bg-indigo-200" : "border-black"} p-2 px-4`}>
-      <button className='mr-4'  onClick={onClick}>Semester {id + 1}</button>
+    <div className={`rounded-full border ${active ? "bg-indigo-200" : "border-black"} p-2 px-4 flex`}>
+      <button className='mr-4' type='button'  onClick={onClick}>Semester {id + 1}</button>
 
-      <button className='align-middle' onClick={handleDeleteSemester}><BsFillTrashFill /></button>
+      <button className='align-middle' type='button' onClick={handleDeleteSemester}><BsFillTrashFill /></button>
     </div>
   )
 }
